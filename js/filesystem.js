ca87835f2eff6d6703430a8d421db09e4424d9e9
Ariful Haque <arifulh@gmail.com>
2012-04-21 05:10:23 -0400

    remove onError

--- js/filesystem.js
+++ js/filesystem.js
@@ -119,36 +119,7 @@ var Filesystem = (function () {
             dir = new DirLoader(dirname, f.filesys);
             dir.destroy(callback);
             return f;
-        },
-
-        onError: function (e) {
-            var f = this;
-            var msg = '';
-
-            switch (e.code) {
-            case FileError.QUOTA_EXCEEDED_ERR:
-                msg = 'QUOTA_EXCEEDED_ERR';
-                break;
-            case FileError.NOT_FOUND_ERR:
-                msg = 'NOT_FOUND_ERR';
-                break;
-            case FileError.SECURITY_ERR:
-                msg = 'SECURITY_ERR';
-                break;
-            case FileError.INVALID_MODIFICATION_ERR:
-                msg = 'INVALID_MODIFICATION_ERR';
-                break;
-            case FileError.INVALID_STATE_ERR:
-                msg = 'INVALID_STATE_ERR';
-                break;
-            default:
-                msg = 'Unknown Error';
-                break;
-            };
-
-            console.log('Error: ' + msg);
         }
-
     }
 
     return Filesystem;
